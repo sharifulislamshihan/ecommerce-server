b666294c74b268a40044f3fdb247aa06d24221b6
unknown <sharifulislamshihan@gmail.com>
2024-03-24 03:26:49 +0600

    pagination and the search filter created

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,46 @@ const User = require('../models/userModel');
 
 
 
-const getUsers = (req, res, next) => {
+const getUsers = async (req, res, next) => {
     try {
-        const  users = await User
+        const search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+
+        const searchRegEx = new RegExp('.*' + search + '.*', 'i');
+
+        // filter to see only users not admin
+        const filter = {
+            isAdmin: { $ne: true },
+            //  name,email or phone should match the search string
+            $or: [
+                { name: { $regex: searchRegEx } },
+                { email: { $regex: searchRegEx } },
+                { phone: { $regex: searchRegEx } },
+            ]
+        }
+        const options = {
+            // skip the password field  from results
+            password: 0
+        }
+        // pagination
+        const users = await User.find(filter, options)
+            .limit(limit)
+            .skip((page - 1) * limit)
+            
+            const  count = await User.find(filter).countDocuments();
+
+            if(!users) throw createError(404,"No Users Found");
+
         res.status(200).json({
             message: 'users were returned',
-            
+            users,
+            pagination: {
+                totalPages: Math.ceil(count/limit),
+                currentPage: page,
+                previousPage: page - 1 > 0 ? page-1 : null,
+                nextPage: page + 1 <= Math.ceil(count/limit) ? page+ 1 : null,
+            }
         })
     }
     catch (error) {
