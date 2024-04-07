53883f6ae17d279baf86b37faa5cf3a32107e0b6
unknown <sharifulislamshihan@gmail.com>
2024-04-08 03:32:34 +0600

    little update in nodemailer

--- helper/email.js
+++ helper/email.js
@@ -31,24 +31,6 @@ const sendEmailWithNodeMailer = async (emailData) => {
         throw error;
     };
 }
-
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-        from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-}
-
-main().catch(console.error);
-
 module.exports = {
     sendEmailWithNodeMailer,
 }
